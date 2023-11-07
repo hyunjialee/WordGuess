a504fb35c5a3fa7169862918c946d976b1c8a8f1
hyunjialee <hyjialee@gmail.com>
2023-11-06 22:05:41 -0500

    pom file update

--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -1,9 +1,13 @@
 package com.github.zipcodewilmington;
-
+import org.junit.*;
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class HangmanTest {
+    @Test
+    public void testMethod(){
+
+    }
 }
--- src/test/java/com/github/zipcodewilmington/WordGuessTest.java
+++ src/test/java/com/github/zipcodewilmington/WordGuessTest.java
@@ -1,4 +1,9 @@
 package com.github.zipcodewilmington;
+import org.junit.*;
 
 public class WordGuessTest {
+    @Test
+    public void testWord (){
+
+    }
 }
